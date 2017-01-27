e17b8696be0245e247c97303d68a600bbe5c1beb
David Li <fantasticmrdavid@gmail.com>
2017-01-27 17:58:07 +1100

    Add Redbubble icon

--- src/client/app/components/View/ContactMedia/index.js
+++ src/client/app/components/View/ContactMedia/index.js
@@ -1,5 +1,6 @@
 import './styles.css';
 import React, { Component } from 'react';
+import Icon from '../../Icon';
 
 export default class ContactMedia extends Component {
   componentDidMount () {
@@ -16,8 +17,11 @@ export default class ContactMedia extends Component {
             <div className="ContactMedia_content">
               <div className="ContactMedia_email"><a href="mailto:fantasticmrdavid@gmail.com">fantasticmrdavid@gmail.com</a></div>
               <div className="ContactMedia_social">
-                <a href="https://au.linkedin.com/in/fantasticmrdavid" target="_blank"><i className="fa fa-linkedIn-square"></i></a>
-                <a href="https://github.com/fantasticmrdavid/" target="_blank"><i className="fa fa-github"></i></a>
+                <a href="https://au.linkedin.com/in/fantasticmrdavid" target="_blank" title="LinkedIn"><i className="fa fa-linkedIn-square"></i></a>
+                <a href="https://github.com/fantasticmrdavid/" target="_blank" title="Github"><i className="fa fa-github"></i></a>
+                <a href="https://www.redbubble.com/people/misterdavid/" target="_blank" title="Redbubble">
+                  <Icon icon="redbubble" />
+                </a>
               </div>
             </div>
           </div>;
