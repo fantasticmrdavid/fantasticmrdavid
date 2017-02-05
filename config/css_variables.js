c3b93990920d2d706c20c2285f90e95c06d76bf0
David Li <fantasticmrdavid@gmail.com>
2017-02-05 16:03:10 +1100

    Add postcss-simple-vars

---
+++ config/css_variables.js
@@ -0,0 +1,17 @@
+module.exports = {
+  'c-light-grey': '#aaa',
+  'c-dark-grey': '#555',
+  'c-charcoal': '#333',
+  'c-dark-charcoal': '#111',
+
+  'l-w-small': '568px',
+  'l-w-medium': '768px',
+  'l-w-large': '1000px',
+
+  'c-text-primary': '#aaa',
+
+  'c-border-color': '#aaa',
+
+  'ff-heading': "'Josefin Slab', Courier, 'Trebuchet MS', 'Times New Roman', serif",
+  'ff-copy': "'Nunito Sans', Helvetica, 'Trebuchet MS', Arial, sans-serif"
+}
--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,13 @@
+var path = require('path');
+var cssVars = require(path.resolve(__dirname, 'config/css_variables'));
+
 module.exports = {
   plugins: [
+    require('postcss-simple-vars')({
+      variables: cssVars
+    }),
     require('postcss-smart-import')({ /* ...options */ }),
     require('precss')({ /* ...options */ }),
     require('autoprefixer')({ /* ...options */ })
-  ]
+  ],
 }
