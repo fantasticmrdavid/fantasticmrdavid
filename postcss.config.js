0bb904eee1d14d09ee79a77affb5b2b1ee3d20c6
David Li <fantasticmrdavid@gmail.com>
2017-03-27 22:24:22 +1100

    Move css_variables.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,5 @@
 var path = require('path');
-var cssVars = require(path.resolve(__dirname, 'config/css_variables'));
+var cssVars = require(path.resolve(__dirname, 'src/client/app/styles/css_variables'));
 
 module.exports = {
   plugins: [
