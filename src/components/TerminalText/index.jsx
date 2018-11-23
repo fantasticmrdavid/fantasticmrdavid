1fe48461efcf32d639de8c9e2dc78a729127d814
David Li <david@kindynow.com>
2018-11-23 14:23:35 +1100

    Add TerminalText to Contact view

---
+++ src/components/TerminalText/index.jsx
@@ -0,0 +1,106 @@
+import React, { Component, Fragment } from 'react';
+import PropTypes from 'prop-types';
+import Blinker from 'components/Blinker';
+
+const LETTER_INTERVAL = 30;
+const COMPLETE_DELAY_INTERVAL = 2000;
+
+class TerminalText extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: <Fragment />,
+      index: 0,
+      letters: props.children.split(''),
+      currentLine: '',
+      complete: false,
+      paused: false,
+    };
+
+    this.boundAddChar = this.addChar.bind(this);
+    this.boundPauseTypewriter = this.pauseTypewriter.bind(this);
+    this.boundStartTypewriter = this.startTypewriter.bind(this);
+    this.boundStartTypewriter();
+  }
+
+  componentDidMount() {
+    this._isMounted = true;
+  }
+
+  componentWillUnmount() {
+    this.boundPauseTypewriter();
+    this._isMounted = false;
+  }
+
+  addChar() {
+    const {
+      complete,
+      currentLine,
+      index,
+      letters,
+      text,
+    } = this.state;
+
+    const {
+      onComplete,
+    } = this.props;
+
+    if (complete) {
+      this.boundPauseTypewriter();
+      setTimeout(() => {
+        if (!!onComplete) onComplete();
+        return true;
+      }, COMPLETE_DELAY_INTERVAL);
+    }
+
+    const nextChar = letters[index];
+
+    return this.setState(index < letters.length ?
+    {
+      text: nextChar === '\n' ? <Fragment>{text}{currentLine}<br /></Fragment> : <Fragment>{text}</Fragment>,
+      currentLine: nextChar === '\n' ? '' : `${currentLine}${nextChar}`,
+      index: index + 1,
+    } : {
+      complete: true,
+    });
+  }
+
+  pauseTypewriter() {
+    clearInterval(this.typewriter);
+    this.setState({ paused: true });
+  }
+
+  startTypewriter() {
+    const { speed } = this.props;
+    const { paused } = this.state;
+    this.typewriter = setInterval(() => {
+      this.boundAddChar();
+    }, speed || LETTER_INTERVAL);
+    if (paused) this.setState({ paused: false });
+  }
+
+  render() {
+    const {
+      complete,
+      currentLine,
+      paused,
+      text,
+    } = this.state;
+    const { blinker } = this.props;
+
+    return (
+      <Fragment>
+        {text}{currentLine}
+        {blinker && <Blinker solid={!complete && !paused} />}
+      </Fragment>);
+  }
+}
+
+TerminalText.propTypes = {
+  children: PropTypes.node.isRequired,
+  blinker: PropTypes.bool,
+  onComplete: PropTypes.func,
+  speed: PropTypes.number,
+};
+
+export default TerminalText;
--- src/views/Contact/index.jsx
+++ src/views/Contact/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import TerminalText from 'components/TerminalText';
 import SocialLink from './SocialLink';
 import socialLinks from './socialLinks';
 import styles from './styles.css';
@@ -9,7 +10,7 @@ export default () => {
       <div className={styles.shutterTop} />
       <div className={styles.shutterBottom} />
       <div className={styles.content}>
-        <div className={styles.email}><a href="mailto:fantasticmrdavid@gmail.com">fantasticmrdavid@gmail.com</a></div>
+        <div className={styles.email}><a href="mailto:fantasticmrdavid@gmail.com"><TerminalText blinker>fantasticmrdavid@gmail.com</TerminalText></a></div>
         <div className={styles.social}>
           { socialLinks.map((l) => {
             return <SocialLink {...l} key={`SocialLink_${l.title}`} />;
