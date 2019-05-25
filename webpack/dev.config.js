b332f5e6ca5907bca898dae6b56962497f656987
David Li <fantasticmrdavid@gmail.com>
2019-05-25 13:31:01 +1000

    Extract common webpack config

--- webpack/dev.config.js
+++ webpack/dev.config.js
@@ -1,55 +1,13 @@
-const webpack = require('webpack');
-const path = require('path');
-// var BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
-
-const BUILD_DIR = path.resolve(__dirname, '../public');
-const APP_DIR = path.resolve(__dirname, '../src');
-
-const env = process.env.NODE_ENV ? process.env.NODE_ENV : false;
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ServiceWorkerWebpackPlugin = require('serviceworker-webpack-plugin');
-const WebpackPwaManifest = require('webpack-pwa-manifest');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const merge = require('webpack-merge');
+const common = require('./common.js');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 process.traceDeprecation = true;
 
-const config = {
-  entry: APP_DIR + '/index.jsx',
+const config = merge(common, {
   mode: 'development',
-  output: {
-    path: BUILD_DIR,
-    filename: 'assets/js/[name]-bundle.js',
-    chunkFilename: 'assets/js/[name]-chunk.js',
-  },
-  resolve: {
-    modules: [APP_DIR, 'node_modules'],
-    extensions: ['.js', '.jsx'],
-    alias: {
-      // https://github.com/webpack/webpack/issues/4666
-      constants: `${APP_DIR}/constants`,
-    }
-  },
   module: {
     rules: [
-      {
-        test: /\.(js|jsx)$/,
-        include: APP_DIR,
-        loader: 'babel-loader'
-      },
-      {
-        test: /\.(js|jsx)$/,
-        enforce: "pre",
-        exclude: /node_modules/,
-        loader: [
-          {
-            loader: 'eslint-loader',
-            options: {
-                failOnWarning: false,
-                failOnError: false
-            }
-          }
-        ]
-      },
       {
         test: /\.html$/,
         use: [
@@ -60,56 +18,15 @@ const config = {
             },
           },
         ],
-      }
-    ]
+      },
+    ],
   },
   devtool: "source-map",
   plugins: [
-    new HtmlWebpackPlugin({
-      template: 'src/index.html',
-      inject: false,
+    new BundleAnalyzerPlugin({
+        analyzerMode: 'static'
     }),
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: JSON.stringify(env)
-      }
-    }),
-    new ServiceWorkerWebpackPlugin({
-      entry: path.join(APP_DIR, 'sw.js'),
-    }),
-    new WebpackPwaManifest({
-      filename: 'manifest.json',
-      name: 'Fantastic Mr David',
-      short_name: 'Fantastic Mr David',
-      description: 'A super amazing portfolio of stuffs!',
-      background_color: '#000',
-      theme_color: '#d12b2b',
-      icons: [
-        {
-          src: 'public/assets/images/pwa_icon.png',
-          sizes: [96, 128, 192, 256, 384, 512] // multiple sizes
-        }
-      ]
-    }),
-    new CopyWebpackPlugin([
-      { from: `${APP_DIR}/styles/noscript.css`, to: 'assets/css/noscript.css' }
-    ]),
-    // new BundleAnalyzerPlugin({
-    //     analyzerMode: 'static'
-    // })
   ],
-  optimization: {
-    runtimeChunk: 'single',
-    splitChunks: {
-      cacheGroups: {
-        vendor: {
-          test: /[\\/]node_modules[\\/]/,
-          name: 'vendor',
-          chunks: 'all',
-        },
-      },
-    },
-  },
-};
+});
 
 module.exports = config;
